c30fa465214a9b44240256aa75f63c8f3e895abb
Anneyino <1132735216@qq.com>
2018-11-28 10:09:29 +0800

    play mode complete

--- MusicBox/MainWindow.xaml.cs
+++ MusicBox/MainWindow.xaml.cs
@@ -31,8 +31,9 @@ namespace MusicBox
         public bool isThreadActive = false;//线程是否处于活跃,用来安全的中止线程;
         MediaPlayer Mainplayer = new MediaPlayer();
         public bool isPlaying = false;//是否正处于播放状态
-        //public bool isLooped = false;//是否处于单曲循环
+        //public bool isUpload = false;//判断是否处于上传音乐的状态,如果是，则在updateSong函数中执行ThreadReset.
         public int totalSeconds = 0;
+        public int currentSeconds = 0;
         public TimeSpan currentTimeSpan = new TimeSpan(0, 0, 0);
         public TimeSpan incrementSpan = new TimeSpan(0, 0, 1);
         public ArrayList songList = new ArrayList();//当前歌单
@@ -56,17 +57,25 @@ namespace MusicBox
 
         private void OpenButton_Click(object sender, RoutedEventArgs e)
         {
-            
+
             //modify part
             //modify part
+            //isUpload = true;
             OpenFileDialog fileDialog = new OpenFileDialog();
             fileDialog.InitialDirectory = "C:\\";
             fileDialog.Filter = "MP3音乐文件|*.mp3|WAV音乐文件|*.wav";
             fileDialog.Title = "选择音乐文件";
             if (fileDialog.ShowDialog() == true)
-            filepath = System.IO.Path.GetFullPath(fileDialog.FileName);
+            {
+                filepath = System.IO.Path.GetFullPath(fileDialog.FileName);
+            }
+            else
+            {
+                filepath = "";
+            }
             if (File.Exists(filepath))
             {
+                
                 updateSong(filepath);
                 /*根据打开的音乐文件创建音乐实例并将其加入当前歌单*/
                 Song song = new Song();
@@ -90,27 +99,44 @@ namespace MusicBox
                 thread = new Thread(
                     new ThreadStart(() =>
                     {
-                        for (int i = 0; i <= totalSeconds; i++)
+                        for (; currentSeconds <= totalSeconds; currentSeconds++)
                         {
-                            this.MusicProgress.Dispatcher.BeginInvoke((ThreadStart)delegate {
-                                this.MusicProgress.Value ++;
-                                currentTimeSpan = currentTimeSpan.Add(incrementSpan);
-                                string str = currentTimeSpan.ToString("mm\\:ss")+"/";
-                                CurrentTimeLabel.Content = str;
-                                if ((i == totalSeconds)&&(mode==playMode.SingleLoop))
+                            this.MusicProgress.Dispatcher.BeginInvoke((ThreadStart)delegate
+                            {
+                                    this.MusicProgress.Value++;
+                                    currentTimeSpan = currentTimeSpan.Add(incrementSpan);
+                                    string str = currentTimeSpan.ToString("mm\\:ss") + "/";
+                                    CurrentTimeLabel.Content = str;
+                                    //Console.WriteLine(thread.ThreadState);
+                                    if ((currentSeconds == totalSeconds) && (mode == playMode.SinglePlay))
+                                    {
+                                        isPlaying = false;
+                                        ControlLabel.Content = "播放";
+                                        ControlButton.Content = "►";
+                                        this.MusicProgress.Value = 0;
+                                        CurrentTimeLabel.Content = "00:00/";
+                                        currentTimeSpan = new TimeSpan(0, 0, 0);
+                                        Mainplayer.Open(new Uri(filepath, UriKind.Absolute));
+                                        Mainplayer.Pause();
+                                        thread = null;
+                                        
+                                    }
+                                 else if((currentSeconds == totalSeconds) && (mode == playMode.SingleLoop))
                                 {
-                                    i = 0;
+                                    currentSeconds = 0;
                                     this.MusicProgress.Value = 0;
                                     currentTimeSpan = new TimeSpan(0, 0, 0);
                                     Mainplayer.Open(new Uri(filepath, UriKind.Absolute));
                                     Mainplayer.Play();
-                                    /*isPlaying = false;
-                                    ControlLabel.Content = "播放";
-                                    ControlButton.Content = "►";
-                                    thread = null;*/
-                                }
+                                }else if((currentSeconds == totalSeconds) && (mode == playMode.ListLoop))
+                                {
+                                    currentSeconds = 0;
+                                    nextSongFunc();
+                                }                                                              
+                                
                             });
                             Thread.Sleep(1000);
+
                         }
                     }));
             }
@@ -124,6 +150,7 @@ namespace MusicBox
                 
                 if ((thread.ThreadState&ThreadState.Unstarted)!=0)
                 {
+                    currentSeconds = 0;
                     thread.Start();
                 }else if(thread.ThreadState == ThreadState.Suspended)
                 {
@@ -191,37 +218,67 @@ namespace MusicBox
 
         private void NextButton_Click(object sender, RoutedEventArgs e)
         {
+            nextSongFunc();
+            Console.WriteLine("?");
+        }
+
+        private void LastButton_Click(object sender, RoutedEventArgs e)
+        {
+            lastSongFunc();
+            Console.WriteLine("!");
+        }
+
+        private void nextSongFunc()
+        {
+            //isUpload = false;
             int SongNo;
             if (songList.Contains(currentSong))
             {
                 SongNo = songList.IndexOf(currentSong);
                 if (SongNo + 1 < songList.Count)
                 {
-                    isPlaying = true;
-                    ControlLabel.Content = "暂停";
-                    ControlButton.Content = "| |";
                     currentSong = (Song)songList[SongNo + 1];
                     updateSong(currentSong.Song_path);
-                    Mainplayer.Play();
+                    /*Mainplayer.Play();
+                    isPlaying = true;
+                    ControlLabel.Content = "暂停";
+                    ControlButton.Content = "| |";*/
+                }
+                else if (SongNo + 1 == songList.Count)
+                {
+                    currentSong = (Song)songList[0];
+                    updateSong(currentSong.Song_path);
+                    /*Mainplayer.Play();
+                    isPlaying = true;
+                    ControlLabel.Content = "暂停";
+                    ControlButton.Content = "| |";*/
                 }
             }
-
         }
-
-        private void LastButton_Click(object sender, RoutedEventArgs e)
+        private void lastSongFunc()
         {
+            //isUpload = false;
             int SongNo;
             if (songList.Contains(currentSong))
             {
                 SongNo = songList.IndexOf(currentSong);
                 if (SongNo > 0)
                 {
-                    isPlaying = true;
-                    ControlLabel.Content = "暂停";
-                    ControlButton.Content = "| |";
                     currentSong = (Song)songList[SongNo - 1];
                     updateSong(currentSong.Song_path);
-                    Mainplayer.Play();
+                    /*Mainplayer.Play();
+                    isPlaying = true;
+                    ControlLabel.Content = "暂停";
+                    ControlButton.Content = "| |";*/
+                }
+                else if (SongNo == 0)
+                {
+                    currentSong = (Song)songList[songList.Count - 1];
+                    updateSong(currentSong.Song_path);
+                    /*Mainplayer.Play();
+                    isPlaying = true;
+                    ControlLabel.Content = "暂停";
+                    ControlButton.Content = "| |";*/
                 }
             }
         }
@@ -248,33 +305,15 @@ namespace MusicBox
             Mainplayer.Open(new Uri(path, UriKind.Absolute));
 
             /*重置数据*/
-
-            if (thread != null)
-            {
-                if (thread.ThreadState != ThreadState.Stopped)
-                {
-                    if (thread.ThreadState == ThreadState.Suspended)
-                    {
-                        thread.Resume();
-                        while (thread.ThreadState == ThreadState.Suspended)
-                        {
-                            Thread.Sleep(100);
-                        }
-                    }
-                    thread.Abort();
-                    while (thread.ThreadState != ThreadState.Aborted)
-                    {
-                        Thread.Sleep(100);
-                    }
-
-                }
-            }
-
-            thread = null;
+            filepath = path;
+            
+            ThreadReset();
+            
             MusicProgress.Value = 0;
             CurrentTimeLabel.Content = "00:00/";
             currentTimeSpan = new TimeSpan(0, 0, 0);
-            totalSeconds = 0;
+            totalSeconds = 10000;
+            currentSeconds = 0;
             isPlaying = false;
             ControlLabel.Content = "播放";
             ControlButton.Content = "►";
@@ -297,6 +336,77 @@ namespace MusicBox
                     break;
                 }
             }
+        }
+
+        private void updateSongWithoutResetThread(string path)
+        {
+            string songTitle = System.IO.Path.GetFileNameWithoutExtension(path);
+            StateLabel.Content = songTitle;
+            Mainplayer.Open(new Uri(path, UriKind.Absolute));
+
+            /*重置数据*/
+            filepath = path;
+
+            MusicProgress.Value = 0;
+            CurrentTimeLabel.Content = "00:00/";
+            currentTimeSpan = new TimeSpan(0, 0, 0);
+            totalSeconds = 10000;
+            isPlaying = false;
+            ControlLabel.Content = "播放";
+            ControlButton.Content = "►";
+            /*重置数据完成*/
+
+            while (true)
+            {
+                ControlButton.IsEnabled = false;
+                ControlButton.Content = "Loading..";
+                if (Mainplayer.NaturalDuration.HasTimeSpan)
+                {
+                    TimeSpan t = Mainplayer.NaturalDuration.TimeSpan;
+
+                    string timestr = t.ToString("mm\\:ss");
+                    totalSeconds = (int)t.TotalSeconds;
+                    MusicProgress.Maximum = totalSeconds;
+                    TotalTimeLabel.Content = timestr;
+                    ControlButton.IsEnabled = true;
+                    ControlButton.Content = "►";
+                    break;
+                }
+            }
+            currentSeconds = 0;
+        }
+
+        private void ThreadReset()
+        {
+            if (thread != null)
+            {
+                if (thread.ThreadState != ThreadState.Stopped)
+                {
+                    if (thread.ThreadState == ThreadState.Suspended)
+                    {
+                        thread.Resume();
+                        while (thread.ThreadState == ThreadState.Suspended)
+                        {
+                            Thread.Sleep(100);
+                        }
+                    }
+                    thread.Abort();
+                    while (thread.ThreadState != ThreadState.Aborted)
+                    {
+                        Thread.Sleep(100);
+                    }
+
+                }
+            }
+            thread = null;
+        }
+
+        private void ShowListButton_Click(object sender, RoutedEventArgs e)
+        {   
+            string path1 = System.IO.Directory.GetCurrentDirectory();
+            path1 += "/SongList/V.A. - 心の海域(オルゴール).mp3";
+            Mainplayer.Open(new Uri(path1, UriKind.Absolute));
+            Mainplayer.Play();
         }
     }
 }
